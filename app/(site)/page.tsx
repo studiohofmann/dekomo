cf0369378d919fa92f7c5cf7a8c333f0013fd896
studiohofmann <peter@studiohofmann.de>
2025-07-02 09:16:40 +0200

    Test auto-deployment after reconnection

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ import Netzwerk from "./components/home/Netzwerk";
 export default function Home() {
   return (
     <div className="page-section">
-      {/* TEST DEPLOYMENT - July 2, 2025 */}
+      {/* TEST DEPLOYMENT #2 - Auto-deploy test */}
       {/*<Einleitung />*/}
       <section className="bg-[#5a7cbe] text-gray-100">
         <Projektbeschreibung />
