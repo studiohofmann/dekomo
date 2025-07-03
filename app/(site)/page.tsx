03b349ec6c2b18fb1e19751015d76a65eda43854
studiohofmann <peter@studiohofmann.de>
2025-07-03 09:40:14 +0200

    Test auto-deployment with working environment variables

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ import Netzwerk from "./components/home/Netzwerk";
 export default function Home() {
   return (
     <div className="page-section">
-      {/* Fresh Vercel project test */}
+      {/* Testing auto-deploy with fresh Vercel project */}
       {/*<Einleitung />*/}
       <section className="bg-[#5a7cbe] text-gray-100">
         <Projektbeschreibung />
