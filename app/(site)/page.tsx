41759e6f5e7dbd589ea3b80f19cfd913fbc5b444
studiohofmann <peter@studiohofmann.de>
2025-07-03 08:58:44 +0200

    Test deployment with private repo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ import Netzwerk from "./components/home/Netzwerk";
 export default function Home() {
   return (
     <div className="page-section">
-      TEST
+      {/* Testing private repo deployment */}
       {/*<Einleitung />*/}
       <section className="bg-[#5a7cbe] text-gray-100">
         <Projektbeschreibung />
