44d712f516491fa2049de25180b2de3627ad016d
studiohofmann <peter@studiohofmann.de>
2025-07-03 08:55:55 +0200

    Deploy test

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,6 +8,7 @@ import Netzwerk from "./components/home/Netzwerk";
 export default function Home() {
   return (
     <div className="page-section">
+      TEST
       {/*<Einleitung />*/}
       <section className="bg-[#5a7cbe] text-gray-100">
         <Projektbeschreibung />
